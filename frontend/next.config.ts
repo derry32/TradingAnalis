98a56e814f94a6277a905af4f7619c81cb70cdcc
BADR Interactive <badrinteractive@192.168.1.12>
2026-07-22 09:43:54 +0700

    fix: proxy API to bypass CORS

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://43.156.79.235:3002/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
